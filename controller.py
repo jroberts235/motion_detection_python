9ac0f124a8031b1d4495247dfdbcdfaf8079f7af
Jeff Roberts <jeff.roberts@thebouqs.com>
2019-05-03 17:23:31 -0700

    updated controller and requirements

--- controller.py
+++ controller.py
@@ -1,20 +1,26 @@
 #! /usr/bin/env python
 
 import sys
-from pyfirmata import Arduino, util
+#from pyfirmata import Arduino, util
+from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
+from signal import pause
 from time import sleep
 
 class Controller:
     def __init__(self, board):
-        self.board = Arduino('/dev/{}'.format(board))
+        #self.board = Arduino('/dev/{}'.format(board))
+        factory = PiGPIOFactory(host='otc-alarm.local')
+        self.alarm = LED(6, pin_factory=factory)
     
     def trigger(self, pin, delay):
-       self.board.digital[pin].write(1) 
+       self.alarm.on()
        sleep(delay)
-       self.board.digital[pin].write(0) 
+       self.alarm.off()
 
     def test(self):
         n = 0
+        led = LED(17)
         while n < 3:
         #    self.board.digital[13].write(1)
         #    print('ON')
@@ -23,10 +29,12 @@ class Controller:
         #    print('OFF')
             for i in range(4,8):
                 print('Pin {}'.format(i))
-                self.board.digital[i].write(1)
+                #self.board.digital[i].write(1)
+                led.on
                 print('ON')
                 sleep(1)
-                self.board.digital[i].write(0)
+                #self.board.digital[i].write(0)
+                led.off
                 print('OFF')
             n += 1
 
